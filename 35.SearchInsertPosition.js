0da44c88f092a7134a20e10018aa37a23ffee541
Alexandr Voishchev <a.voishchev@outlook.com>
2018-10-30 13:09:39 -0600

    10/30/2018 update

--- 35.SearchInsertPosition.js
+++ 35.SearchInsertPosition.js
@@ -16,6 +16,7 @@ var searchInsert = function(nums, target) {
   return(i);
 };
 
+// tests
 console.log(searchInsert([1,3,5,6], 5))
 console.log(searchInsert([1,3,5,6], 2))
 console.log(searchInsert([1,3,5,6], 7))
